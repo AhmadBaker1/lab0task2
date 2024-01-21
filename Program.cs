a84a80c3117f93dc0cab1854698f8e31c26258c2
Ahmad Baker <ahmadqadoomy79@gmail.com>
2024-01-20 18:25:29 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab0task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowNumber, highNumber;
+
+            // Looping and Input Validation
+            do
+            {
+                Console.WriteLine("Enter a positive low number");
+            } while (!int.TryParse(Console.ReadLine(), out lowNumber) || lowNumber <= 0);
+
+            do
+            {
+                Console.WriteLine("Enter a high number");
+            } while (!int.TryParse(Console.ReadLine(), out highNumber) || highNumber <= 0);
+        }
+    }
+}
